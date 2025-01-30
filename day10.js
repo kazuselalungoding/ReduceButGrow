ac3568f5aa1e1d754476ee97f2139dcbc82d36f3
kazuselalungoding <manchabadboy5gmail.com>
2025-01-30 14:30:01 +0700

    Perlahan Tapi Pasti

---
+++ day10.js
@@ -0,0 +1,3 @@
+//https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+
+const removeChar = (str) => str.substring(1, str.length -1)
---
+++ day9.js
@@ -0,0 +1,7 @@
+function validateHello(greetings) {
+
+    if (typeof res === 'string' || res instanceof String) {
+        return console.log(ada)
+    }
+    return
+  }
